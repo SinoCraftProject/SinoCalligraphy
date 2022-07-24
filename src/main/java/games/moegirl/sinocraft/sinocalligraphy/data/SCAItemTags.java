ccb56e400fb1466c419f2d91cc9306d03119cf3d
秋雨落 <i@rain.cx>
2022-07-24 18:24:55 +0800

    fix: typo with nocaet.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/games/moegirl/sinocraft/sinocalligraphy/data/SCAItemTags.java
+++ src/main/java/games/moegirl/sinocraft/sinocalligraphy/data/SCAItemTags.java
@@ -1,7 +1,6 @@
 package games.moegirl.sinocraft.sinocalligraphy.data;
 
 import games.moegirl.sinocraft.sinocalligraphy.SinoCalligraphy;
-import games.moegirl.sinocraft.sinocore.SinoCore;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
@@ -11,5 +10,5 @@ public class SCAItemTags {
     public static final TagKey<Item> INKS = ItemTags.create(new ResourceLocation(SinoCalligraphy.MODID, "inks"));
     public static final TagKey<Item> FAN = ItemTags.create(new ResourceLocation(SinoCalligraphy.MODID, "fan"));
 
-    public static final TagKey<Item> GARLIC = ItemTags.create(new ResourceLocation("noceat", "flames"));
+    public static final TagKey<Item> GARLIC = ItemTags.create(new ResourceLocation("nocaet", "flames"));
 }
