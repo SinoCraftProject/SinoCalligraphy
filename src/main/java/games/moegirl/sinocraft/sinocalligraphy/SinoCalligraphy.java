3e4922deb46e8d8352bcb1c474ea6bc5ef41eaa2
秋雨落 <i@rain.cx>
2022-07-28 19:25:15 +0800

    feat: log messages.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/games/moegirl/sinocraft/sinocalligraphy/SinoCalligraphy.java
+++ src/main/java/games/moegirl/sinocraft/sinocalligraphy/SinoCalligraphy.java
@@ -21,11 +21,15 @@ public class SinoCalligraphy {
     public static final Map<String, Object> DEBUG = new HashMap<>();
 
     public static final String MODID = "sinocalligraphy";
-    public static final String VERSION = "1.18.2-1.0.2";
+    public static final String MC_VERSION = "1.18.2";
+    public static final String MOD_VERSION = "1.0.3";
+    public static final String VERSION = MC_VERSION + "-" + MOD_VERSION;
     private static final Logger LOGGER = LogManager.getLogger();
 
     public SinoCalligraphy() {
-        LOGGER.info("Loading SinoCalligraphy.");
+        LOGGER.info("Loading SinoCalligraphy. Ver: " + MOD_VERSION);
+
+        LOGGER.info("The painting that used color to depict all walks of life, while retained black and white to render the beauty of ages.");
 
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
