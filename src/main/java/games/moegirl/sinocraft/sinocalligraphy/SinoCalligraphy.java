45bd85c7640d75eab269328198d0685d03ef4d2c
luqin2007 <lq2007lq@hotmail.com>
2022-09-13 15:30:47 +0800

    new branch

--- src/main/java/games/moegirl/sinocraft/sinocalligraphy/SinoCalligraphy.java
+++ src/main/java/games/moegirl/sinocraft/sinocalligraphy/SinoCalligraphy.java
@@ -26,7 +26,7 @@ public class SinoCalligraphy {
 
     public static final String MODID = "sinocalligraphy";
     public static final String MC_VERSION = "1.18.2";
-    public static final String MOD_VERSION = "1.1.1";
+    public static final String MOD_VERSION = "2.0.0-alpha";
     public static final String VERSION = MC_VERSION + "-" + MOD_VERSION;
     private static final Logger LOGGER = LogManager.getLogger();
 
